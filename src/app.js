f56cb8802bf9918898c3010f830273667666763c
Fernando Sartorius <92523759+Sartorius11@users.noreply.github.com>
2024-12-18 19:21:58 +0000

    Ejercicio corregido Clase

--- src/app.js
+++ src/app.js
@@ -1,23 +1,23 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 
 let pronoun = ['the', 'our'];
-let adj = ['great', 'big'];
+let adjective = ['great', 'big'];
 let noun = ['jogger', 'racoon'];
 let listForLoop = '';
 
 for (let i = 0; i < pronoun.length; i++) {
-  for (let j = 0; j < adj.length; j++) {
+  for (let j = 0; j < adjective.length; j++) {
     for (let k = 0; k < noun.length; k++) {
       listForLoop += `
-          <li class="list-group-item">
-            ${pronoun[i]}${adj[j]}${noun[k]}
-          </li>`
+        <li class="list-group-item">
+            ${pronoun[i]}${adjective[j]}${noun[k]}.com
+        </li>
+      `
     }
   }
 }
